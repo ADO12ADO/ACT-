d36102c84d7e7c3e1d1563a3b4d0bbf125d1a658
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-06-25 22:02:30 +0800

    Update discord.js

--- discord-bitget-trading-bot/lib/discord.js
+++ discord-bitget-trading-bot/lib/discord.js
@@ -3,8 +3,6 @@ const placeOrder = require('./bitget');
 const parseMessage = require('./parser');
 require('dotenv').config();
 
-console.log("Initializing Discord Client...");
-
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -23,7 +21,6 @@ client.on('messageCreate', async (message) => {
         console.log('Message received:', message.content);
         const { token, entryPrice, invalidationLevel, targetPrices } = parseMessage(message.content);
 
-        // Assume we fetch balance from some API, for simplicity let's use a constant value
         const balance = 10000; // Example balance
         await placeOrder(token, entryPrice, invalidationLevel, targetPrices, balance);
     }
@@ -35,7 +32,7 @@ function startBot() {
         .then(() => console.log("Bot started successfully"))
         .catch(err => {
             console.error("Error starting bot:", err);
-            throw err; // Throw the error to be caught in the API handler
+            throw err; 
         });
 }
 
@@ -45,9 +42,8 @@ function stopBot() {
         .then(() => console.log("Bot stopped successfully"))
         .catch(err => {
             console.error("Error stopping bot:", err);
-            throw err; // Throw the error to be caught in the API handler
+            throw err; 
         });
 }
 
 module.exports = { startBot, stopBot, client };
-
