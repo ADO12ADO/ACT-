241929f75dabc587d137ba44448208b02f70c037
ADO <118599888+ADO12ADO@users.noreply.github.com>
2024-06-25 12:14:09 +0800

    Update start-bot.js

--- discord-bitget-trading-bot/pages/api/start-bot.js
+++ discord-bitget-trading-bot/pages/api/start-bot.js
@@ -1,10 +1,10 @@
 // pages/api/start-bot.js
-import client from '../../lib/discord';
+import { startBot, client } from '../../lib/discord';
 
 export default function handler(req, res) {
     if (req.method === 'POST') {
         if (!client.isReady()) {
-            client.login(process.env.DISCORD_BOT_TOKEN).then(() => {
+            startBot().then(() => {
                 res.status(200).json({ message: 'Bot started' });
             }).catch((error) => {
                 res.status(500).json({ error: 'Failed to start bot', details: error });
